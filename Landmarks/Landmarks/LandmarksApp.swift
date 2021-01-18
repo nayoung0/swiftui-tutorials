80083c8c5e0d50777aa838888e9eab6ba2f0ef85
nayoung <nayoung.tech@gmail.com>
2021-01-19 00:04:35 +0900

    1. Creating and Combining Views

---
+++ Landmarks/Landmarks/LandmarksApp.swift
@@ -0,0 +1,22 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by inpock on 2021/01/18.
+//
+
+import SwiftUI
+
+// An app that uses the SwiftUI app life cycle has a structure
+// that conforms to the App protocol. The structure’s body property
+// returns one or more scenes, which in turn provide content for display.
+// @main The @main attribute identifies the app’s entry point.
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
